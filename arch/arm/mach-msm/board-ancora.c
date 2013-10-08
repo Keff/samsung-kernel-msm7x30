087d556117e35f61eb705d638beaff0a2032d415
Keff <funnykeff91@gmail.com>
2013-10-08 22:35:06 +0800

    ancora: added slightly higher memory(ram) support

--- arch/arm/mach-msm/board-ancora.c
+++ arch/arm/mach-msm/board-ancora.c
@@ -146,7 +146,11 @@ EXPORT_SYMBOL(sec_class);
 struct device *switch_dev;
 EXPORT_SYMBOL(switch_dev);
 
-#define MSM_PMEM_SF_SIZE	0x1A00000
+#ifdef CONFIG_HIGH_MEMORY
+#define MSM_PMEM_SF_SIZE	0x1100000 // 17mb
+#else
+#define MSM_PMEM_SF_SIZE	0x1A00000 // 26mb
+#endif
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
 #define MSM_FB_PRIM_BUF_SIZE	(800 * 480 * 4 * 3) /* 4bpp * 3 Pages */
 #else
@@ -155,9 +159,9 @@ EXPORT_SYMBOL(switch_dev);
 
 #define MSM_FB_SIZE roundup(MSM_FB_PRIM_BUF_SIZE, 4096)
 
-#define MSM_PMEM_ADSP_SIZE		0x2800000
-#define MSM_FLUID_PMEM_ADSP_SIZE	0x2800000
-#define PMEM_KERNEL_EBI0_SIZE		0x600000
+#define MSM_PMEM_ADSP_SIZE		0x2800000 // 40mb
+#define MSM_FLUID_PMEM_ADSP_SIZE	0x2800000 // 40mb
+#define PMEM_KERNEL_EBI0_SIZE		0x600000  // 6mb
 
 #define PMIC_GPIO_INT		27
 #define PMIC_VREG_WLAN_LEVEL	2900
